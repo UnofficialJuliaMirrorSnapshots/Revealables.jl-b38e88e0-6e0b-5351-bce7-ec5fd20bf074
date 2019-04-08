193d1f5028e81078d9538ea56825f5028ec88061
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 02:11:04 +0000

    Snapshot of branch master taken on 2019-04-07T22:11:03.54-04:00 from "https://github.com/UnofficialJuliaMirror/Revealables.jl-b38e88e0-6e0b-5351-bce7-ec5fd20bf074"

---
+++ README.md
@@ -0,0 +1,31 @@
+# Revealables
+
+This module allows Markdown (and most HTML) to be hidden and revealed by pressing a button in an Jupyter notebook. You can also encode and decode the content using a Caesar cipher.
+
+`Revealables` is useful for hiding and revealing answers to practice problems. When you choose, you can give students the password so they can decode the answers.
+
+## Installation
+```julia
+Pkg.add("Revealables")
+```
+
+You will generally want to use Revealables.jl with the [hide_input](http://jupyter-contrib-nbextensions.readthedocs.io/en/latest/nbextensions/hide_input/readme.html) Jupyter extension installed. This extension hides the code cells. Revealables will reveal the output when a button is pressed. If the extension is not installed, then the code will not be hidden. You can unhide the code cells by clicking on the chevron-up icon.
+
+Instructions for installing Jupyter extensions can be found [here](https://github.com/ipython-contrib/jupyter_contrib_nbextensions/wiki/Home-4.x-(Jupyter)). If you are using [anaconda](https://www.continuum.io/downloads), which supplies the [conda](https://conda.io/docs/) command, you can run the following:
+```
+conda install -c conda-forge jupyter_contrib_nbextensions
+```
+Then you must run:
+```
+jupyter contrib nbextension install --user
+jupyter nbextension enable hide_input/main
+```
+
+## Use
+Hide and show blocks of Markdown through code like this:
+
+```julia
+revealable("""Any Markdown can go here!""", "Button Label")
+```
+
+Please see the [example notebook](doc/example.ipynb) for more detail.
---
+++ src/Revealables.jl
@@ -0,0 +1,98 @@
+__precompile__()
+
+module Revealables
+
+using Interact
+using Reactive
+
+export Revealable
+export revealable
+export encode!
+export encode
+export decode!
+export decode
+
+
+type Revealable
+    markdown::String
+    label::String
+    show::Bool
+    encoded::Bool
+end
+
+Revealable(markdown::String, label::String = "Show/Hide", show = false, encoded = false) = Revealable(markdown, label, show, encoded)
+
+function revealable(markdown::String, label::String, show::Bool)
+    x = Revealable(markdown, label, show)
+    revealable(x)
+end
+
+function revealable(markdown::String, label::String = "Show/Hide")
+    x = Revealable(markdown, label, false)
+    revealable(x)
+end
+
+function revealable(x::Revealable)
+    @manipulate for n in togglebutton(; label = x.label, value=x.show, signal=Signal(x.show))
+        x.show = n
+        x
+    end
+end
+
+
+function rot(text::String, key::Int)
+    map(text) do c
+        if 'a' <= c <= 'z'
+            Char(mod(c - 'a' + key, 26) + 'a')
+        elseif 'A' <= c <= 'Z'
+            Char(mod(c - 'A' + key, 26) + 'A')
+        elseif '&' <= c <= '?'
+            Char(mod(c - '&' + key, 26) + '&')
+        else
+            c
+        end
+    end
+end
+
+function encode!(x::Revealable, password::String)
+    if x.encoded
+        x
+    else
+        key = sum(map(x->convert(Int, x), collect(password)))
+        x.markdown = rot(x.markdown, key)
+        x.encoded = true
+        x
+    end
+end
+
+function encode(x::Revealable, password::String)
+    r = Revealable(x.markdown, x.label, x.show)
+    encode!(r, password)
+end
+
+function decode!(x::Revealable, password::String)
+    if x.encoded
+        key = 26 - sum(map(x->convert(Int, x), collect(password)))
+        x.markdown = x.markdown = rot(x.markdown, key)
+        x.encoded = false
+        x
+    else
+        x
+    end
+end
+
+function decode(x::Revealable, password::String)
+    r = Revealable(x.markdown, x.label, x.show)
+    decode!(r, password)
+end
+
+
+function Base.show(io::IO, ::MIME"text/markdown", x::Revealable)
+    if x.show
+        println(io, x.markdown)
+    else
+        println(io, "")
+    end
+end
+
+end # module
